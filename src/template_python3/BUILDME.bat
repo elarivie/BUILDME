1139e2bf01641cf8b35f653d82f4a1fa72e3fe6e
Éric Larivière <ericlariviere@hotmail.com>
2015-05-27 20:00:41 -0400

    Fix python version in batch file

--- src/template_python3/BUILDME.bat
+++ src/template_python3/BUILDME.bat
@@ -3,6 +3,6 @@ REM The current file:
 REM     Should be named "BUILDME.bat"
 REM     Should be present in the project root folder
 REM The python executable should be in the PATH
-python -B BUILDME %*
+python3 -B BUILDME %*
 EXIT /B %ERRORLEVEL%
 
